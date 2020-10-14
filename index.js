e00911d1c03567f04d7aa09a5bdb63463e4964e2
CyanGuan <72875049+CyanGuan@users.noreply.github.com>
2020-10-14 23:14:53 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var dailyObjects = ["MacBook Pro", "AirPods", "Mug", "Muji Shoes"]
+console.log(dailyObjects)
+console.log(dailyObjects[0])
+
+var myLaptop =
+{
+  "name": "MacBook Pro",
+  "productionDate": "2015",
+  "color": "silver",
+  "StoreLocation": "Hong Kong",
+  "System": "macOS Catalina"
+};
+console.log(myLaptop);
+console.log(myLaptop.color);
