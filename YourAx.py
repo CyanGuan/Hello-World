a995ae9bec7b05e44a41f8d993aba16db42cef2f
CyanGuan <72875049+CyanGuan@users.noreply.github.com>
2020-10-22 00:54:48 +0800

    Add files via upload

---
+++ YourAx.py
@@ -0,0 +1,21 @@
+import random
+
+axs = ['wood','iron','silver','gold','diamond']
+YourAx = random.choice(axs)
+
+for i in range(3):
+    print ('RiverGod: There are {} axs I found in the river.'.format(axs))
+    guess = input('Which one is yours:')
+    if guess != YourAx:
+        print('RiverGod: Seem you lied, {} is not your ax!'.format(guess))
+    else:
+        break
+
+if guess == YourAx:
+    print('I am glad you are a honest person. Now, these four axs are yours!')
+else:
+    print('Unfortunatly, you lied too much time. So you lose your axs foever.')
+
+
+
+
