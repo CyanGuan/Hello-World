2409b91f2a888d94f0877b1029d4c43fe596674f
CyanGuan <72875049+CyanGuan@users.noreply.github.com>
2020-10-29 01:48:41 +0800

    Add files via upload

---
+++ week4_loop.py
@@ -0,0 +1,35 @@
+from turtle import *
+import random
+
+pencolor('white')
+goto(0,200)
+colors = [(1, 0.38, 0.27), (1, 0.71, 0.75), (1, 1, 0.5), (0.68, 0.93, 0.93), (0.88, 0.88, 0.88)]
+pensize(0)
+
+angel = 60
+distance = 50
+
+for i in range(10):
+    for i in range(10):
+        for i in range(3):
+            fill = random.choice(colors)
+            fillcolor(fill)
+            pencolor('black')
+            begin_fill()
+            forward(50)
+            left(60)
+            forward(50)
+            left(60)
+            forward(50)
+            left(60)
+            forward(50)
+            right(60)
+            end_fill()
+
+        forward(50)
+        right(60)
+        forward(50)
+
+    forward(50)
+    right(angel + 60)
+    forward(distance + 50)
